d16ea1444dc31ff0bb4cf8833dc08b8d040a2969
Tatu Nordström <Tatu.Nordstrom@metropolia.fi>
2020-11-24 11:27:37 +0200

    osa 4 valmis

---
+++ osa4/controllers/blogs.js
@@ -0,0 +1,58 @@
+const blogsRouter = require('express').Router()
+const jwt = require('jsonwebtoken')
+const Blog = require('../models/blog')
+const User = require('../models/user')
+
+blogsRouter.get('/', async (request, response) => {
+  const blogs = await Blog.find({})
+  .populate('user', { username: 1, name: 1 })
+  response.json(blogs.map(blog => blog.toJSON()))
+})
+  
+blogsRouter.post('/', async (request, response) => {
+  const body = request.body
+  const decodedToken = jwt.verify(request.token, process.env.SECRET)
+  const user = await User.findById(decodedToken.id)
+
+  const blog = new Blog ({
+  title: body.title,
+  author: body.author,
+  url: body.url,
+  likes: body.likes === undefined ? 0 : body.likes,
+  user: user._id
+  })
+
+  const savedBlog = await blog.save()
+  user.blogs = user.blogs.concat(savedBlog._id)
+  await user.save()
+  response.status(201).json(savedBlog.toJSON())    
+})
+
+blogsRouter.delete('/:id', async (request, response) => {
+  const decodedToken = jwt.verify(request.token, process.env.SECRET)
+  const user = await User.findById(decodedToken.id)
+  const blog = await Blog.findById(request.params.id)
+
+  if(user._id.toString() === blog.user.toString()) {
+    await Blog.deleteOne(blog)
+    response.status(204).end()
+  } else {
+    response.status(401).send({ error: 'You can delete only your own added blogs'})
+  }
+})
+
+blogsRouter.put('/:id', async (request,response) => {
+  const body = request.body
+
+  const blog = {
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes
+  }
+  const updatedBlog = await Blog.findOneAndUpdate(request.params.id, blog, { new: true, runValidators: true, context: 'query' })
+  response.json(updatedBlog.toJSON())
+})
+
+
+module.exports = blogsRouter
---
+++ osa4/utils/middleware.js
@@ -0,0 +1,48 @@
+const e = require('express')
+const logger = require ('./logger')
+
+const requestLogger = (request, response, next) => {
+    logger.info('Method:', request.method)
+    logger.info('Path:  ', request.path)
+    logger.info('Body:  ', request.body)
+    logger.info('---')
+    next()
+}
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+}
+
+const errorHandler = (error, request, response, next) => {
+  logger.error(error.message)
+  
+  if (error.name === 'CastError' && error.kind === 'ObjectId') {
+    return response.status(400).send({ error: 'malformatted id' })
+    } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })  
+  } else if (error.name === 'JsonWebTokenError') {
+    return response.status(401).json({ error: 'invalid token' })
+  } 
+  
+  logger.error(error.message)
+  next(error)
+}
+
+const tokenExtractor = (request, response, next) => {
+
+  if(request.method != 'GET'  && request.path.startsWith('/api/blogs')) {
+    const authorization = request.get('authorization')
+    if(authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    request.token = authorization.substring(7)
+  } else {
+    return response.status(401).json({ error: 'token missing' })
+  }
+}
+  next()
+}
+module.exports = {
+errorHandler,
+requestLogger,
+unknownEndpoint,
+tokenExtractor
+}
